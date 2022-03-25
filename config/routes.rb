3e6c301102bccef993d84f96085146571159f7b4
Peter Sampson <petersampson010@gmail.com>
2022-03-25 13:09:04 +0000

    add root route444

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'admin_users#index'
+  root 'admin_user#index'
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
