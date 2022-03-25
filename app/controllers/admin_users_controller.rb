63d52385a12fc1b32e2b6feb2f33befb01a5b641
Peter Sampson <petersampson010@gmail.com>
2022-03-25 13:22:22 +0000

    added welcome

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,2 +1,6 @@
 class AdminUsersController < ApplicationController
+
+    def welcome 
+        render json: "Welcome to SLFF BACKEND API"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'admin_user#index'
+  root 'admin_users#welcome'
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
