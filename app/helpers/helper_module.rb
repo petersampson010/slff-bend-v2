7e6bfc163a5cc50eb0d2f0f923f1f7e4397f2a48
Peter Sampson <petersampson010@gmail.com>
2022-03-30 14:33:07 +0100

    add auth

--- app/helpers/helper_module.rb
+++ app/helpers/helper_module.rb
@@ -36,27 +36,27 @@ module HelperModule
         token = auth_header.split(' ').last if auth_header
         puts 'auth token: ' + token
         puts 'are we hitting'
-        # begin
-        #     puts 'decode here'
-        #     puts JWT.decode('eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbl91c2VyX2lkIjoxLCJleHAiOjE2NDkyNDM0NDV9.BQgpcUfhX3lVcbB30WErzDuumaI4hWLifj_1W5NT9A8', Rails.application.secrets.secret_key_base, 'HS256')
-        #     @decoded = jwt_decode(token)
-        #     puts 'decoded'
-        #     puts @decoded
-        #     if @decoded["user_id"] 
-        #         @current_user = User.find(@decoded["user_id"])
-        #     else 
-        #         @current_user = AdminUser.find(@decoded["admin_user_id"])
-        #     end
-        # rescue ActiveRecord::RecordNotFound => e
-        #     puts  e
-        #     puts e.message
-        #     render json: { errors: e.message }, status: :unauthorized
-        # rescue JWT::DecodeError => e
-        #     puts  e
-        #     puts e.message
+        begin
+            puts 'decode here'
+            # puts JWT.decode('eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbl91c2VyX2lkIjoxLCJleHAiOjE2NDkyNDM0NDV9.BQgpcUfhX3lVcbB30WErzDuumaI4hWLifj_1W5NT9A8', Rails.application.secrets.secret_key_base, 'HS256')
+            @decoded = jwt_decode(token)
+            puts 'decoded'
+            puts @decoded
+            if @decoded["user_id"] 
+                @current_user = User.find(@decoded["user_id"])
+            else 
+                @current_user = AdminUser.find(@decoded["admin_user_id"])
+            end
+        rescue ActiveRecord::RecordNotFound => e
+            puts  e
+            puts e.message
+            render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            puts  e
+            puts e.message
 
-        #     render json: { errors: e.message }, status: :unauthorized
-        # end
+            render json: { errors: e.message }, status: :unauthorized
+        end
     end 
 
 end
