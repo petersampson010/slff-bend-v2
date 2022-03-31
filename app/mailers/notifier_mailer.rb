c6fa294d205d45b765d2a422952d3bdd4210e459
Peter Sampson <petersampson010@gmail.com>
2022-03-31 09:31:02 +0100

    1

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -8,6 +8,7 @@ class NotifierMailer < ApplicationMailer
     def au_registration_confirmation(admin_user)
         puts 'inside au_reg_conf'
         puts admin_user.email
+        puts ENV["SET_API_URL"]
         mail(:to => "#{admin_user.email}", :subject => "Registration Confirmation")
     end
 
